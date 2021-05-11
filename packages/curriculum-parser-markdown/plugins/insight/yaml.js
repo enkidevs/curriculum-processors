8eabe4bcfcf7ff5a54f2dc109a33842d0161ec41
kapnobatai137 <andrei@enki.com>
2021-05-11 18:21:01 +0100

    :bug: fix parsing for new link syntax

--- packages/curriculum-parser-markdown/plugins/insight/yaml.js
+++ packages/curriculum-parser-markdown/plugins/insight/yaml.js
@@ -31,7 +31,32 @@ module.exports = function yaml() {
 const mdUrlRegEx = /\[(.*)\]\((.*)\)/;
 const mdUrlRegExWithType = /\[(.*)\]\((.*)\)\{(.*)\}/;
 
+// accounts for any spaces or newlines inside the name, url, nature or outside
+// this string:
+// [Difference between Primary and Foreign
+//   Keys]
+//   (https://www.dotnettricks.com/learn/sqlserver/difference-between-primary-key-and-foreign-key){article}
+// returns three groups
+// match.groups = {
+//   name: "Difference between Primary and Foreign  Keys", (the space is not a typo, we also handle that)
+//   url: "https://www.dotnettricks.com/learn/sqlserver/difference-between-primary-key-and-foreign-key",
+//   nature: "article"
+// }
+const yamlUrlRegExWithType = /\[(?<name>[^\[\]]*)\]\s*\((?<url>[^\(\)]*)\)\s*\{(?<nature>[^\{\}]*)\}/;
+
 function getMarkdownLink(link) {
+  if (yamlUrlRegExWithType.test(link)) {
+    const match = yamlUrlRegExWithType.exec(link);
+    const { name, url, nature } = match.groups;
+
+    // remove any extra spaces before returning
+    return {
+      name: removeExtraSpaces(name),
+      url: removeExtraSpaces(url),
+      nature: removeExtraSpaces(nature),
+    };
+  }
+
   if (mdUrlRegExWithType.test(link)) {
     const [, name, url, nature] = mdUrlRegExWithType.exec(link);
     return { name, url, nature };
@@ -57,4 +82,8 @@ function getDomainFromURL(url) {
         // find & remove port number
         .split(':')
     : null;
+}
+
+function removeExtraSpaces(string) {
+  return string.replace(/\s+/g, ' ').trim();
 }
